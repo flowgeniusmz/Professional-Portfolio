35a7df3df3dbcb3e0667cbd4f8c2003c60ce1bc5
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2024-01-07 06:00:48 +0000

    initial

---
+++ constant.py
@@ -0,0 +1,29 @@
+info = {
+   "Pronoun": "him", 
+   "Name": "Michael",
+   "Full_Name": "Michael Zozulia",
+   "Intro": "Father, Innovator, and Technologist | President and CEO of FlowGenius, Inc.",
+   "About":"Disrupting the way we solve business problems.",
+   "Tableau":"https://public.tableau.com/app/profile/vickytc/viz/SunnybrookTeam/Overview",
+   "Medium":"https://medium.com/@vicky-note/about",
+   "City":"Chicago, IL",
+   "Photo":"""<a href=\"https://www.linkedin.com/in/michaelzozulia/\"><img src=\"https://media.licdn.com/dms/image/D5603AQFccN88Fj97_Q/profile-displayphoto-shrink_400_400/0/1700590933720?e=1710374400&v=beta&t=2bv2EMs22dRDOPOXbdZk1LyW_O3WtHxl6jUqg2--vMY" width=\"200\"   alt=\"Profile\" title=\"Profile\"></a>""",
+   "Email": "mzozulia@flowgenius.com"
+}
+
+embed_rss= {
+    'rss':"""<div style="overflow-y: scroll; height:500px; background-color:white;"> <div id="retainable-rss-embed" 
+        data-rss="https://medium.com/feed/@vicky-note"
+        data-maxcols="3" 
+        data-layout="grid"
+        data-poststyle="inline" 
+        data-readmore="Read the rest" 
+        data-buttonclass="btn btn-primary" 
+        data-offset="0"></div></div> <script src="https://www.twilik.com/assets/retainable/rss-embed/retainable-rss-embed.js"></script>"""
+}
+
+endorsements = {
+    "img1": "https://user-images.githubusercontent.com/90204593/238169843-12872392-f2f1-40a6-a353-c06a2fa602c5.png",
+    "img2": "https://user-images.githubusercontent.com/90204593/238171251-5f4c5597-84d4-4b4b-803c-afe74e739070.png",
+    "img3": "https://user-images.githubusercontent.com/90204593/238171242-53f7ceb3-1a71-4726-a7f5-67721419fef8.png"
+}
